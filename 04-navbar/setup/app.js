c1f493e8d70f93fc2d3a579416fa0bb8877d3298
ivyhu <ajianouc@gmail.com>
2022-06-05 22:03:54 -0700

    04-navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.getElementsByClassName("nav-toggle")[0];
+const links = document.getElementsByClassName("links")[0];
+
+navToggle.addEventListener("click", () => {
+  // if (!links.classList.contains("show-links")) {
+  //   links.classList.add("show-links");
+  // } else {
+  //   links.classList.remove("show-links");
+  // }
+  links.classList.toggle("show-links");
+})
